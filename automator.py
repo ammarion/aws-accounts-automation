53e8ded82b075fee1ed34faafe929b6e25a29273
Ammar Alim <ammar.alim@nordstrom.com>
2019-04-13 20:35:55 -0700

    personal accounts changes

--- automator.py
+++ automator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""python3 create_account.py --account_name ate-io-product54 --account_email ate-io-product54@pearson.com"""
+"""python3 create_account.py --account_name createonaws@gmail.com --account_email createonaws@@gmail.com"""
 
 import boto3
 import botocore
@@ -259,150 +259,150 @@ def main(arguments):
 
     # Enable enterprise support
 
-    def enable_enterpris():
-        client = session.client('support')
+    # def enable_enterpris():
+    #     client = session.client('support')
+    #
+    #     response = client.create_case(
+    #         subject='Enable Enterprise Support Plan',
+    #         serviceCode='customer-account',
+    #         severityCode='normal',
+    #         categoryCode='activation',
+    #         communicationBody='Hello AWS, please enable the Enterprise Support Plan for account {}\
+    #         which is a member of our Organization'.format(accountid),
+    #         ccEmailAddresses=[
+    #             'ammar.osman.ali@gmail.com',
+    #         ],
+    #         language='en',
+    #         issueType='customer-service'
+    #     )
+    #     print(response)
 
-        response = client.create_case(
-            subject='Enable Enterprise Support Plan',
-            serviceCode='customer-account',
-            severityCode='normal',
-            categoryCode='activation',
-            communicationBody='Hello AWS, please enable the Enterprise Support Plan for account {}\
-            which is a member of our Organization'.format(accountid),
-            ccEmailAddresses=[
-                'ammar.alim@pearson.com', 'ate-io-finops@pearson.com',
-            ],
-            language='en',
-            issueType='customer-service'
-        )
-        print(response)
+    #
+    #
+    # def assume_role_iam_nord():
+    #
+    #     """
+    #         Assume admin role within the nord-IAM account and return credentials
+    #     """
+    #     s = boto3.Session(profile_name='nord')
+    #     sts_client = s.client('sts')
+    #     role_arn = 'arn:aws:iam::812653090533:role/managed-role/nordAdmin'
+    #
+    #     # Call the assume_role method of the STSConnection object and pass the role
+    #     # ARN and a role session name.
+    #
+    #     assuming_role = True
+    #     while assuming_role is True:
+    #         try:
+    #             assuming_role = False
+    #             assumedroleobject = sts_client.assume_role(
+    #                 RoleArn=role_arn,
+    #                 RoleSessionName="IAM-nordAccountRole"
+    #             )
+    #         except botocore.exceptions.ClientError as e:
+    #             assuming_role = True
+    #             print(e)
+    #             print("Retrying...")
+    #             time.sleep(10)
+    #
+    #     # From the response that contains the assumed role, get the temporary
+    #     # credentials that can be used to make subsequent API calls
+    #     return assumedroleobject['Credentials']
+    #
+    # # Create groups in iam-nord account
+    #
+    # def get_groups_template(template_groups_file):
+    #
+    #     """
+    #         Read a template file and return the contents
+    #     """
+    #
+    #     print("Reading resources from " + template_groups_file)
+    #     f = open(template_groups_file, "r")
+    #     iam_groups_template = f.read()
+    #     return iam_groups_template
+    #
+    # def deploy_groups(credentials, template, stack_name, stack_region, AWSAccountNumber):
+    #
+    #     """
+    #         Create a CloudFormation stack of resources within the new account
+    #     """
+    #
+    #     datestamp = time.strftime("%d/%m/%Y")
+    #     client = boto3.client('cloudformation',
+    #                           aws_access_key_id=credentials['AccessKeyId'],
+    #                           aws_secret_access_key=credentials['SecretAccessKey'],
+    #                           aws_session_token=credentials['SessionToken'],
+    #                           region_name=stack_region)
+    #     print("Creating stack " + stack_name + " in " + stack_region)
+    #
+    #     creating_stack = True
+    #     while creating_stack is True:
+    #         try:
+    #             creating_stack = False
+    #             create_stack_response = client.create_stack(
+    #                 StackName=stack_name,
+    #                 TemplateBody=template,
+    #                 Parameters=[
+    #                     {
+    #                         'ParameterKey': 'AWSAccountNumber',
+    #                         'ParameterValue': accountid,
+    #                         'UsePreviousValue': False
+    #                     },
+    #
+    #                 ],
+    #                 NotificationARNs=[],
+    #                 Capabilities=[
+    #                     'CAPABILITY_NAMED_IAM',
+    #                 ],
+    #                 OnFailure='ROLLBACK',
+    #                 Tags=[
+    #                     {
+    #                         'Key': 'ManagedResource',
+    #                         'Value': 'True'
+    #                     },
+    #                     {
+    #                         'Key': 'DeployDate',
+    #                         'Value': datestamp
+    #                     }
+    #                 ]
+    #             )
+    #         except botocore.exceptions.ClientError as e:
+    #             creating_stack = True
+    #             print(e)
+    #             print("Retrying...")
+    #             time.sleep(30)
+    #
+    #     stack_building = True
+    #     print("Stack creation in process...")
+    #     print(create_stack_response)
+    #     while stack_building is True:
+    #         event_list = client.describe_stack_events(StackName=stack_name).get("StackEvents")
+    #         stack_event = event_list[0]
+    #
+    #         if (stack_event.get('ResourceType') == 'AWS::CloudFormation::Stack' and
+    #                 stack_event.get('ResourceStatus') == 'CREATE_COMPLETE'):
+    #             stack_building = False
+    #             print("Stack construction complete.")
+    #         elif (stack_event.get('ResourceType') == 'AWS::CloudFormation::Stack' and
+    #               stack_event.get('ResourceStatus') == 'ROLLBACK_COMPLETE'):
+    #             stack_building = False
+    #             print("Stack construction failed.")
+    #             sys.exit(1)
+    #         else:
+    #             print(stack_event)
+    #             print("Stack building . . .")
+    #             time.sleep(10)
+    #
+    #     stack = client.describe_stacks(StackName=stack_name)
+    #     return stack
 
-
-
-    def assume_role_iam_pearson():
-
-        """
-            Assume admin role within the Pearson-IAM account and return credentials
-        """
-        s = boto3.Session(profile_name='pearson')
-        sts_client = s.client('sts')
-        role_arn = 'arn:aws:iam::812653090533:role/managed-role/PearsonAdmin'
-
-        # Call the assume_role method of the STSConnection object and pass the role
-        # ARN and a role session name.
-
-        assuming_role = True
-        while assuming_role is True:
-            try:
-                assuming_role = False
-                assumedroleobject = sts_client.assume_role(
-                    RoleArn=role_arn,
-                    RoleSessionName="IAM-PearsonAccountRole"
-                )
-            except botocore.exceptions.ClientError as e:
-                assuming_role = True
-                print(e)
-                print("Retrying...")
-                time.sleep(10)
-
-        # From the response that contains the assumed role, get the temporary
-        # credentials that can be used to make subsequent API calls
-        return assumedroleobject['Credentials']
-
-    # Create groups in iam-pearson account
-
-    def get_groups_template(template_groups_file):
-
-        """
-            Read a template file and return the contents
-        """
-
-        print("Reading resources from " + template_groups_file)
-        f = open(template_groups_file, "r")
-        iam_groups_template = f.read()
-        return iam_groups_template
-
-    def deploy_groups(credentials, template, stack_name, stack_region, AWSAccountNumber):
-
-        """
-            Create a CloudFormation stack of resources within the new account
-        """
-
-        datestamp = time.strftime("%d/%m/%Y")
-        client = boto3.client('cloudformation',
-                              aws_access_key_id=credentials['AccessKeyId'],
-                              aws_secret_access_key=credentials['SecretAccessKey'],
-                              aws_session_token=credentials['SessionToken'],
-                              region_name=stack_region)
-        print("Creating stack " + stack_name + " in " + stack_region)
-
-        creating_stack = True
-        while creating_stack is True:
-            try:
-                creating_stack = False
-                create_stack_response = client.create_stack(
-                    StackName=stack_name,
-                    TemplateBody=template,
-                    Parameters=[
-                        {
-                            'ParameterKey': 'AWSAccountNumber',
-                            'ParameterValue': accountid,
-                            'UsePreviousValue': False
-                        },
-
-                    ],
-                    NotificationARNs=[],
-                    Capabilities=[
-                        'CAPABILITY_NAMED_IAM',
-                    ],
-                    OnFailure='ROLLBACK',
-                    Tags=[
-                        {
-                            'Key': 'ManagedResource',
-                            'Value': 'True'
-                        },
-                        {
-                            'Key': 'DeployDate',
-                            'Value': datestamp
-                        }
-                    ]
-                )
-            except botocore.exceptions.ClientError as e:
-                creating_stack = True
-                print(e)
-                print("Retrying...")
-                time.sleep(30)
-
-        stack_building = True
-        print("Stack creation in process...")
-        print(create_stack_response)
-        while stack_building is True:
-            event_list = client.describe_stack_events(StackName=stack_name).get("StackEvents")
-            stack_event = event_list[0]
-
-            if (stack_event.get('ResourceType') == 'AWS::CloudFormation::Stack' and
-                    stack_event.get('ResourceStatus') == 'CREATE_COMPLETE'):
-                stack_building = False
-                print("Stack construction complete.")
-            elif (stack_event.get('ResourceType') == 'AWS::CloudFormation::Stack' and
-                  stack_event.get('ResourceStatus') == 'ROLLBACK_COMPLETE'):
-                stack_building = False
-                print("Stack construction failed.")
-                sys.exit(1)
-            else:
-                print(stack_event)
-                print("Stack building . . .")
-                time.sleep(10)
-
-        stack = client.describe_stacks(StackName=stack_name)
-        return stack
-
-    enable_enterpris()
-    credential = assume_role_iam_pearson()
-    groups_template = get_groups_template('groups.yml')
-    deploy_groups(credential, template=groups_template, stack_name='pearson-iam-groups-' + accountid,
-                  stack_region='us-east-1', AWSAccountNumber=accountid)
-
+    # enable_enterpris()
+    # credential = assume_role_iam_nord()
+    # groups_template = get_groups_template('groups.yml')
+    # deploy_groups(credential, template=groups_template, stack_name='nord-iam-groups-' + accountid,
+    #               stack_region='us-east-1', AWSAccountNumber=accountid)
+    #
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
